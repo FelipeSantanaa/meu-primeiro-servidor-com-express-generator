a197ea8c413c7b08e0137e209d19a8bb438ff1df
Felipe Santana <felipe008_@hotmail.com>
2021-08-30 20:40:31 -0300

    Att readme

---
+++ server/routes/users.js
@@ -0,0 +1,8 @@
+const express = require('express'),
+    router = express.Router(),
+     userController = require('../controllers/users')
+
+
+router.get('/', userController.index);
+
+module.exports = router;
