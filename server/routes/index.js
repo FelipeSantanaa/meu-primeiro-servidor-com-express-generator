cc1cb877d08ac1c2571f6920d1513d493f44d9d3
Felipe Santana <felipe008_@hotmail.com>
2021-08-30 21:49:04 -0300

    feature: Adicionado usuarios e view que percorre o array

--- server/routes/index.js
+++ server/routes/index.js
@@ -3,10 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', {
-    titulo: 'do Jefferson',
-    subtitulo: 'Site maneiro'
-  });
+  res.render('index', { title: 'Express' });
 });
 
 module.exports = router;
--- server/routes/users.js
+++ server/routes/users.js
@@ -1,8 +1,9 @@
 const express = require('express'),
-    router = express.Router(),
-     userController = require('../controllers/users')
+  router = express.Router(),
+  userController = require('../controllers/users')
 
+  router.get('/doideira', userController.chuparCanaEAssobiar)
+  router.get('/:apelido', userController.show)
+  router.get('/', userController.index)
 
-router.get('/', userController.index);
-
-module.exports = router;
+module.exports = router
